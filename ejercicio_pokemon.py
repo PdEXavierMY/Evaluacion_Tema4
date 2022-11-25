f7ff8f41aae49e3118a4c94aaabd96ecb6dfbc7b
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-11-25 17:01:29 +0100

    apartado b

--- ejercicio_pokemon.py
+++ ejercicio_pokemon.py
@@ -4,3 +4,10 @@ class Pokemon(object):
         self.numero = numero
         self.tipo = tipo
         self.debilidad = debilidad
+
+
+def inorden_numero(raiz):
+            if(raiz is not None):
+                inorden_numero(raiz.izq)
+                print(raiz.info[1], raiz.info[0])
+                inorden_numero(raiz.der)
